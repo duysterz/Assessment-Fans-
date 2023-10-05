a0de9db27c473dd635a804a1ad4649a9fc67ecf2
duysterz <106291099+duysterz@users.noreply.github.com>
2023-10-05 08:30:42 -0700

    Adding more styling, sharing updates

--- Front_End/src/App.js
+++ Front_End/src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Home from './components/Home';  
+import About from './components/About'; 
+import Contact from './components/Contact';
 // import ImageGallery from './components/ImageGallery'; 
 // import FilterButtons from './components/FilterButtons'; 
 
@@ -11,6 +14,11 @@ function App() {
       <div className="App">
         <Header />
         <main className="container">
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
           {/* <FilterButtons /> 
           <Routes>
             <Route path="/" element={<ImageGallery />} /> 
