d97be352a1b59f3ea85ec842eb693d258f23c826
nickmdo95 <nmmontesdeoca@gmail.com>
2023-10-05 15:19:12 -0400

    Modified Room model

--- Back_End/fan-site-server/src/main/java/learn/fan_site/models/Room.java
+++ Back_End/fan-site-server/src/main/java/learn/fan_site/models/Room.java
@@ -1,5 +1,10 @@
 package learn.fan_site.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,9 +13,22 @@ import org.hibernate.validator.constraints.URL;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class Room {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int roomId;
+
+    @NotBlank(message = "Room color required.")
     private String roomColor;
+
+    @NotBlank(message = "Room style required.")
     private String roomStyle;
+
+    @NotBlank(message = "Room type required.")
     private String roomType;
+
+    @URL(message = "Valid room image URL required.")
+    @NotBlank(message = "Room image URL required.")
+    private String imageUrl;
 }
