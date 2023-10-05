54df25b820efa5412afcc32e3f780af84988aeca
nickmdo95 <nmmontesdeoca@gmail.com>
2023-10-05 14:34:51 -0400

    Modified Room Model

--- Back_End/fan-site-server/src/main/java/learn/fan_site/models/Room.java
+++ Back_End/fan-site-server/src/main/java/learn/fan_site/models/Room.java
@@ -1,47 +1,16 @@
 package learn.fan_site.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.URL;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Room {
     private int roomId;
-    private int typeId;
-    private int styleId;
-    private int colorId;
-
-    public Room(int roomId, int typeId, int styleId, int colorId) {
-        this.roomId = roomId;
-        this.typeId = typeId;
-        this.styleId = styleId;
-        this.colorId = colorId;
-    }
-
-    public int getRoomId() {
-        return roomId;
-    }
-
-    public void setRoomId(int roomId) {
-        this.roomId = roomId;
-    }
-
-    public int getTypeId() {
-        return typeId;
-    }
-
-    public void setTypeId(int typeId) {
-        this.typeId = typeId;
-    }
-
-    public int getStyleId() {
-        return styleId;
-    }
-
-    public void setStyleId(int styleId) {
-        this.styleId = styleId;
-    }
-
-    public int getColorId() {
-        return colorId;
-    }
-
-    public void setColorId(int colorId) {
-        this.colorId = colorId;
-    }
+    private String roomColor;
+    private String roomStyle;
+    private String roomType;
 }
