c1c9657dc1faf0dddc75e2e771685eae26e646c7
nickmdo95 <nmmontesdeoca@gmail.com>
2023-10-06 11:52:45 -0400

    Added @Repository annotation to RoomRepository class

--- Back_End/fan-site-server/src/main/java/learn/fan_site/data/RoomRepository.java
+++ Back_End/fan-site-server/src/main/java/learn/fan_site/data/RoomRepository.java
@@ -2,6 +2,8 @@ package learn.fan_site.data;
 
 import learn.fan_site.models.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RoomRepository extends JpaRepository<Room, Integer> {
 }
