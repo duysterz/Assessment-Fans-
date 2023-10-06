0c7d74b1c9d743bc8c592d6e908dbb2dd28b8e35
Sreya-25 <25.sreya.25@gmail.com>
2023-10-06 10:24:12 -0400

    added RoomService & RoomController

--- Back_End/fan-site-server/src/main/java/learn/fan_site/controllers/RoomController.java
+++ Back_End/fan-site-server/src/main/java/learn/fan_site/controllers/RoomController.java
@@ -1,4 +1,86 @@
 package learn.fan_site.controllers;
 
+import learn.fan_site.domain.RoomService;
+import learn.fan_site.domain.Result;
+import learn.fan_site.domain.ResultType;
+import learn.fan_site.models.Room;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import jakarta.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api/room")
+
 public class RoomController {
+    private final RoomService roomService;
+
+    public RoomController(RoomService roomService) {
+        this.roomService = roomService;
+    }
+
+    @GetMapping
+    public List<Room> findAll() {
+        return roomService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> findById(@PathVariable int id) {
+        Room room = roomService.findById(id);
+        if (room != null) {
+            return new ResponseEntity<>(room, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> add(@RequestBody @Valid Room room, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return buildInvalidResponse(bindingResult);
+        }
+        Result<Room> result = roomService.add(room);
+        if (result.isSuccess()) {
+            return new ResponseEntity<>(result.getPayload(), HttpStatus.CREATED);
+        }
+        return new ResponseEntity<>(result.getMessages(), HttpStatus.BAD_REQUEST);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> update(@PathVariable int id,
+                                         @RequestBody @Valid Room room,
+                                         BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return buildInvalidResponse(bindingResult);
+        }
+        if (id != room.getRoomId()) {
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
+        Result<Void> result = roomService.update(room);
+        if (result.isSuccess()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } else if (result.getResultType() == ResultType.NOT_FOUND) {
+            return new ResponseEntity<>(result.getMessages(), HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(result.getMessages(), HttpStatus.BAD_REQUEST);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deleteById(@PathVariable int id) {
+        Result<Void> result = roomService.deleteById(id);
+        if (result.isSuccess()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(result.getMessages(), HttpStatus.NOT_FOUND);
+    }
+
+    private ResponseEntity<Object> buildInvalidResponse(BindingResult bindingResult) {
+        return new ResponseEntity<>(bindingResult.getAllErrors().stream()
+                .map(i -> i.getDefaultMessage())
+                .collect(Collectors.toList()), HttpStatus.BAD_REQUEST);
+    }
+
 }
--- Back_End/fan-site-server/src/main/java/learn/fan_site/domain/RoomService.java
+++ Back_End/fan-site-server/src/main/java/learn/fan_site/domain/RoomService.java
@@ -1,4 +1,80 @@
 package learn.fan_site.domain;
 
+import learn.fan_site.data.RoomRepository;
+import learn.fan_site.models.Room;
+import org.springframework.stereotype.Service;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validator;
+import java.util.List;
+
+
+@Service
 public class RoomService {
+
+    private final RoomRepository roomRepository;
+    private final Validator validator;
+
+    public RoomService(RoomRepository roomRepository, Validator validator) {
+        this.roomRepository = roomRepository;
+        this.validator = validator;
+    }
+
+    public List<Room> findAll() {
+        return roomRepository.findAll();
+    }
+
+    public Room findById(int roomId) {
+        return roomRepository.findById(roomId).orElse(null);
+    }
+
+    public Result<Room> add(Room room) {
+        Result<Room> result = validate(room);
+        if (!result.isSuccess()) {
+            return result;
+        }
+
+        room = roomRepository.save(room);
+        result.setPayload(room);
+        return result;
+    }
+
+    public Result<Void> update(Room room) {
+        Result<Void> result = validate(room);
+        if (!result.isSuccess()) {
+            return result;
+        }
+        if (findById(room.getRoomId()) != null) {
+            roomRepository.save(room);
+            return result;
+        }
+        result.addMessage(String.format("Room id: '%s' not found.", room.getRoomId()), ResultType.NOT_FOUND);
+        return result;
+    }
+
+    public Result<Void> deleteById(int roomId) {
+        Result<Void> result = new Result<>();
+        if (findById(roomId) != null) {
+            roomRepository.deleteById(roomId);
+            return result;
+        }
+        result.addMessage(String.format("Room id: '%s' not found.", roomId), ResultType.NOT_FOUND);
+        return result;
+    }
+
+    private <T> Result<T> validate(Room room) {
+        Result<T> result = new Result<>();
+
+        if (room == null) {
+            result.addMessage("Room cannot be null.", ResultType.INVALID);
+            return result;
+        }
+
+        var violations = validator.validate(room);
+        for (ConstraintViolation<Room> violation : violations) {
+            result.addMessage(violation.getMessage(), ResultType.INVALID);
+        }
+
+        return result;
+    }
 }
