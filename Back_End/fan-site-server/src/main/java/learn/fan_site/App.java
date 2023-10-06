2a882fe9014e2fe611629f5c7138b4c601fa0dd2
Sreya-25 <25.sreya.25@gmail.com>
2023-10-06 12:42:06 -0400

    added springboot annotation to App class

--- Back_End/fan-site-server/src/main/java/learn/fan_site/App.java
+++ Back_End/fan-site-server/src/main/java/learn/fan_site/App.java
@@ -1,9 +1,18 @@
 package learn.fan_site;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+//
+//@EnableAutoConfiguration
+@SpringBootApplication
 public class App {
+
     public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
+        SpringApplication app = new SpringApplication(App.class);
+        app.run(args);
     }
+
 }
