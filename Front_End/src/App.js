50d8ed3bfc8434feb2ba58136f8358addfdad5bc
duysterz <106291099+duysterz@users.noreply.github.com>
2023-10-09 07:18:54 -0700

    Halting footer

--- Front_End/src/App.js
+++ Front_End/src/App.js
@@ -2,34 +2,35 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Home from './components/Home';  
-import About from './components/About'; 
+import Home from './components/Home';
+import About from './components/About';
 import Contact from './components/Contact';
 import Upload from './components/Upload';
-// import ImageGallery from './components/ImageGallery'; 
-// import FilterButtons from './components/FilterButtons'; 
 
 function App() {
   return (
     <Router>
-      <div className="App">
+      <div className="App" style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>  {/* Style added here */}
         <Header />
-        <main className="container">
-        <Routes>
+        <main className="container" style={{ flex: 1 }}>  {/* Style added here */}
+          <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/contact" element={<Contact />} />
             <Route path="/upload" element={<Upload />} />
           </Routes>
-          {/* <FilterButtons /> 
-          <Routes>
-            <Route path="/" element={<ImageGallery />} /> 
-          </Routes> */}
         </main>
-        <Footer />
+        {/* <Footer /> */}
       </div>
     </Router>
   );
 }
 
 export default App;
+
+
+
+    {/* <FilterButtons /> 
+          <Routes>
+            <Route path="/" element={<ImageGallery />} /> 
+          </Routes> */}
