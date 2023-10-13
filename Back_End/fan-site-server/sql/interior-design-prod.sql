d0879aaba948137782a742125c06946d30ee9735
nickmdo95 <nmmontesdeoca@gmail.com>
2023-10-13 14:18:58 -0400

    Added app-security schema

--- Back_End/fan-site-server/sql/interior-design-prod.sql
+++ Back_End/fan-site-server/sql/interior-design-prod.sql
@@ -1,5 +1,6 @@
 drop database if exists interior_design;
 create database interior_design;
+
 use interior_design;
 
 create table room (
@@ -8,33 +9,4 @@ create table room (
     room_style varchar(50) not null,
     room_type varchar(50) not null,
     image_url varchar(250) not null
-);
-
-drop table if exists app_user_role;
-drop table if exists app_role;
-drop table if exists app_user;
-
-create table app_user (
-    app_user_id int primary key auto_increment,
-    username varchar(50) not null unique,
-    password_hash varchar(2048) not null,
-    enabled bit not null default(1)
-);
-
-create table app_role (
-    app_role_id int primary key auto_increment,
-    `name` varchar(50) not null unique
-);
-
-create table app_user_role (
-    app_user_id int not null,
-    app_role_id int not null,
-    constraint pk_app_user_role
-        primary key (app_user_id, app_role_id),
-    constraint fk_app_user_role_user_id
-        foreign key (app_user_id)
-        references app_user(app_user_id),
-    constraint fk_app_user_role_role_id
-        foreign key (app_role_id)
-        references app_role(app_role_id)
 );
