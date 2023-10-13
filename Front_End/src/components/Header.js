5b3cc7bf54426a849115a38bf9f9504644fa4c14
duysterz <106291099+duysterz@users.noreply.github.com>
2023-10-13 07:49:49 -0700

    Header: Conditional Links and Logout Button.

--- Front_End/src/components/Header.js
+++ Front_End/src/components/Header.js
@@ -1,22 +1,40 @@
-import React from 'react';
-import { Link } from 'react-router-dom';  
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import '../App.css';
 
 const Header = () => {
+  const [user, setUser] = useState(null);
+
+  const handleLogout = () => {
+    setUser(null);  // Reset user -> null on logout
+    // more logout logic?
+  };
+
   return (
     <header className="header">
-      <h1> Divine Flip Designs </h1>
+      <h1>Divine Flip Designs</h1>
       <nav>
         <ul>
-          <li><Link to="/">Home</Link></li> 
-          <li><Link to="/about">About</Link></li> 
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
           <li><Link to="/upload">Upload</Link></li>
           <li><Link to="/contact">Contact</Link></li>
-          
+          {/* Conditionally render links */}
+          {user === null ? (
+            <>
+              <li><Link to="/login">Login</Link></li>
+              <li><Link to="/register">Register</Link></li>
+            </>
+          ) : (
+            <>
+              <li>{user.username}</li>
+              <li><button onClick={handleLogout}>Logout</button></li>
+            </>
+          )}
         </ul>
       </nav>
     </header>
   );
 };
 
-export default Header;
+export default Header;;
